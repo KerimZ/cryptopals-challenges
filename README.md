2101bf6d2d89e0dbcb204a33f03c114273df4e63
Kerim Zunic <kerim@zunic.me>
2022-03-25 16:00:23 +0100

    Add base64 challenge directory

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Cryptopals-Set-1
+
+Solutions for [Set 1](https://cryptopals.com/sets/1) of the [Cryptopals Challenges](https://cryptopals.com/).
+1. Base64
---
+++ base64/src/main.rs
@@ -0,0 +1,21 @@
+
+
+fn main() {
+    let input_value:&str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected:&str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+    println!("Input: {}", input_value);
+    println!("Base64: {}", base64(input_value));
+}
+
+fn base64(input_value:&str) -> &str {
+
+    // Convert to binary representation
+
+    // Operate
+
+    // Convert back to String and return
+
+
+    return input_value;
+}
